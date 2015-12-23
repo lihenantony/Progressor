a8c8686817780d895ccbc5c89cc198476d06048b
llihenantony <lihenantony1997@gmail.com>
2015-12-22 18:49:18 -0800

    model task added

---
+++ Progressor/Progressor/Models/Task.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace Progressor.Models
+{
+    public class Task
+    {
+        public string taskID { get; set; }
+        public string userID { get; set; }
+        public string name { get; set; }
+        public int progressIndex { get; set; }
+        public int progressMax { get; set; }
+        public DateTime dueDate { get; set; }
+        public DateTime createDate { get; set; }
+        public DateTime startDate { get; set; }
+        public DateTime completeDate { get; set; }
+        public int difficultyIndex { get; set; }
+        public int importanceIndex { get; set; }
+        public string taskStatus { get; set; }
+
+    }
+
+    public class TaskDBContext : DbContext
+    {
+        public DbSet<Task> Tasks { get; set; }
+    }
+}

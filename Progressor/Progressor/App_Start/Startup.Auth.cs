1ed92fecb9a55f0d25672a98c10a1f27fe4fc167
llihenantony <lihenantony1997@gmail.com>
2015-12-22 17:04:39 -0800

    auth log in with mysql db

--- Progressor/Progressor/App_Start/Startup.Auth.cs
+++ Progressor/Progressor/App_Start/Startup.Auth.cs
@@ -38,12 +38,12 @@ namespace Progressor
             app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
 
             // Enables the application to temporarily store user information when they are verifying the second factor in the two-factor authentication process.
-            //app.UseTwoFactorSignInCookie(DefaultAuthenticationTypes.TwoFactorCookie, TimeSpan.FromMinutes(5));
+            app.UseTwoFactorSignInCookie(DefaultAuthenticationTypes.TwoFactorCookie, TimeSpan.FromMinutes(5));
 
             // Enables the application to remember the second login verification factor such as phone or email.
             // Once you check this option, your second step of verification during the login process will be remembered on the device where you logged in from.
             // This is similar to the RememberMe option when you log in.
-            //app.UseTwoFactorRememberBrowserCookie(DefaultAuthenticationTypes.TwoFactorRememberBrowserCookie);
+            app.UseTwoFactorRememberBrowserCookie(DefaultAuthenticationTypes.TwoFactorRememberBrowserCookie);
 
             // Uncomment the following lines to enable logging in with third party login providers
             //app.UseMicrosoftAccountAuthentication(
@@ -56,7 +56,7 @@ namespace Progressor
 
             app.UseFacebookAuthentication(
                appId: "1129241220420047",
-               appSecret: "●●●●●●●●");
+               appSecret: "5dc1a4c51255c8a8073159cb293481a6");
 
             //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
             //{
